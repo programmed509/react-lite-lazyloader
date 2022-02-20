749a3d1d149dd7bf769709516ca7acaf0c15ca13
Prachidipta Mishra <neosoft@pop-os.localdomain>
2022-02-20 15:19:20 +0530

    created component for LiteLazyLoader

---
+++ LiteLazyLoader.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+const LiteLazyLoad = ({ children, startLoadingfrom, percentVisible }) => {
+  const elementRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const options = {
+    root: null,
+    rootMargin: startLoadingfrom ?? "0px",
+    threshold: percentVisible,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      if (entry.isIntersecting) {
+        setIsVisible(entry.isIntersecting);
+        observer.unobserve(elementRef.current);
+      }
+    }, options);
+    if (elementRef.current) observer.observe(elementRef.current);
+
+    return () => {
+      if (elementRef.current) observer.unobserve(elementRef.current);
+    };
+  }, [elementRef, options]);
+  return (
+    <div id="lazy-loaded" ref={elementRef}>
+      {isVisible && children}
+    </div>
+  );
+};
+export default LiteLazyLoad;
